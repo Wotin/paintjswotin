3c055c9463df780cbcfb7e016477fae548bbb054
simplepro <simplepro1254@gmail.com>
2020-08-30 16:15:39 +0900

    PaintJSWotin is done!

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fill_btn = document.getElementById("jsMode");
 const save_btn = document.getElementById("jsSave");
 const current_color = document.getElementById("current_color")
 const current_color_text = document.getElementById("current_color_text")
+const eraser_btn = document.getElementById("eraser_canvas_btn")
 
 const INITIAL_COLOR = "2c2c2c"
 const CANVAS_SIZE = 700
@@ -90,6 +91,12 @@ function clickedSaveBtn(event) {
     link.click()
 }
 
+function clickedEraser(event) {
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+    ctx.fillStyle = color
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -111,4 +118,8 @@ if(fill_btn){
 
 if(save_btn) {
     save_btn.addEventListener("click", clickedSaveBtn)
+}
+
+if(eraser_btn) {
+    eraser_btn.addEventListener("click", clickedEraser)
 }
